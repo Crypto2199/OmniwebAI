d28ed069594c11e8aae728cd3bdcc850bdd7cb0a
Crypto2199 <cryptodz2199@gmail.com>
2025-07-24 11:39:26 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import Game from './core/Game.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const game = new Game();
+    
+    // يمكنك الوصول إلى كل الميزات من هنا:
+    // game.board
+    // game.ai
+    // game.timer
+    // game.achievements
+});
